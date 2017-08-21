a5ff9870ec1a25fdfcd6ad4677104e097090f20e
Dominik Kurbiel <github email address>
2017-08-22 00:24:55 +0200

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-$(document).ready(function (){
+$(document).ready(function () {
 });
+	function searchRepositories() {
+		var username = document.getElementById('searchTerms').value;
+		var gitUrl = `https://api.github.com/users/${username}/repos`;
+		
+		$('#results').empty();
+		$('#errors').empty();
+		
+		$.get(gitUrl)
+			.done(data => {
+				const src = $('#repository-template').html();
+				const template = Handlebars.compile(src);
+				
+				$("#results").html(template(data));
+		})
+		.fail(error => {
+			displayError()
+		})
+	};
+
+function showCommits(element){
+	const repoName = element.dataset.repository;
+	const userName = element.dataset.username;
+	const repoLink = `https://api.github.com/repos/${userName}/${repoName}/commits`;
+	
+	$('#details').empty();
+	$('#errors').empty();
+	
+	$.get(repoLink)
+		.done(data => {
+		const src = $('#repo-commits-template').html();
+		const template = Handlebars.compile(src);
+				
+		$("#details").html(template(data));
+	})
+}
+
+function displayError() {
+	$("#errors").html(`Something went wrong! Error message: ${error.responseJSON.message}`)
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+	Handlebars.registerPartial("authorPartial", $('#author-partial-template').html());
+})
